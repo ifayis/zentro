56ee50f4de1995bd42f642e77faeeda04ff13e78
Fayis KV <mohammedfayis135@gmail.com>
2025-08-09 18:15:42 +0530

    z

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 function About() {
   const navigate = useNavigate()
   return (
-    <section className="min-h-screen bg-gradient-to-br from-[#f0f4f8] to-[#dce3ea] flex items-center justify-center px-6">
-      <div className="max-w-3xl text-center bg-white p-10 rounded-3xl shadow-2xl border border-gray-200">
+    <section className="min-h-screen bg-gradient-to-br from-[#4d0a6ccd] to-[#000000] flex items-center justify-center px-6">
+      <div className="max-w-3xl text-center bg-[#ede6efcd] p-10 rounded-3xl shadow-2xl border border-gray-200">
         <h2 className="text-4xl sm:text-5xl font-extrabold text-gray-900 mb-6 tracking-tight">
           About <span className="text-blue-600">Zentro App</span>
         </h2>
